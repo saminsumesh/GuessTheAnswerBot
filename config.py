c54887845674975516f350ad4e497a94ee4531db
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2022-02-12 19:19:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,17 +3,17 @@ from os import environ
 
 API_ID = int(os.environ.get("API_ID")
 
-API_HASH = os.environ.get("API_HASH")
+API_HASH = environ.get("API_HASH")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_TOKEN = environ.get("BOT_TOKEN")
 
 START_TXT = """Hey {}, Iam a Guess Game Bot 
 
 Hope You Enjoyed The Bot
 """
 
-GUESS_COUNT = int(os.environ.get("GUESS_COUNT", "")
+GUESS_COUNT = int(os.environ.get("GUESS_COUNT", ""))
 
-MAX_GUESSES = int(os.environ.get("MAX_GUESSES", "")
+MAX_GUESSES = int(os.environ.get("MAX_GUESSES", ""))
 
-SECRET_WORD = os.envrion.get("Any word that you want your users to guess")
+SECRET_WORD = envrion.get("Any word that you want your users to guess")
